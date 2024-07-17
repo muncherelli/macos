b521db88035dbb46d34c3d9fee919d24e3a8a830
muncherelli <16467105+muncherelli@users.noreply.github.com>
2024-07-17 14:52:33 -0500

    remove pass unless needed

--- update.sh
+++ update.sh
@@ -11,4 +11,6 @@ fi
 ansible-galaxy install -r requirements.yml
 
 # run ansible update playbook
-ansible-playbook update.yml --ask-become-pass
+# ansible-playbook update.yml --ask-become-pass
+ansible-playbook update.yml
+
